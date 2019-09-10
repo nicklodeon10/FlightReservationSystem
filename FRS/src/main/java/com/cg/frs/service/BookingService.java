910aebc77f8bf0bcbab5dff694cbc50c946e67bb
nicklodeon10 <10.devang@gmail.com>
2019-09-10 23:43:12 +0530

    Added More Validations.

--- FRS/src/main/java/com/cg/frs/service/BookingService.java
+++ FRS/src/main/java/com/cg/frs/service/BookingService.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.cg.frs.dto.Booking;
 import com.cg.frs.dto.Passenger;
+import com.cg.frs.dto.ScheduleFlight;
 import com.cg.frs.exception.FRSException;
 
 public interface BookingService {
@@ -18,6 +19,10 @@ public interface BookingService {
 	public Booking modifyBooking(Booking booking);
 	
 	public void deleteBooking(BigInteger bookingId);
+	
+	public Integer modifySeatCount(ScheduleFlight scheduleFlight, int change) ;
+	
+	public Integer validatePassengerCount(ScheduleFlight scheduleFlight,Integer passengerChange) throws FRSException;
 	
 	public BigInteger validateBookingWithId(BigInteger bookingId) throws FRSException;
 	

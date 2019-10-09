791f255ea7a1b81e4afd7d29ac724b1f6797e796
nicklodeon10 <10.devang@gmail.com>
2019-10-10 00:14:26 +0530

    Added Logback

--- FRS-Sprint5/src/main/java/com/cg/frs/FlightReservationSystemApplication.java
+++ FRS-Sprint5/src/main/java/com/cg/frs/FlightReservationSystemApplication.java
@@ -1,13 +1,18 @@
 package com.cg.frs;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FlightReservationSystemApplication {
+	
+	private static final Logger logger = LoggerFactory.getLogger(FlightReservationSystemApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlightReservationSystemApplication.class, args);
+		logger.info("Running.");
 	}
 
 }
